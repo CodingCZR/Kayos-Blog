edb76dfed375debbef6f613e09aa70d82af17eca
jaquellsensi <146892715+jaquellsensi@users.noreply.github.com>
2023-12-28 10:06:24 -0700

    Go

--- microbloglite-capstone-starter-frontend/posts/posts.js
+++ microbloglite-capstone-starter-frontend/posts/posts.js
@@ -2,6 +2,7 @@
 
 //"use strict";
 
+<<<<<<< HEAD
 // Function to post a tweet
 function makePost() {
     // Retrieve the tweet content from the modal input field
@@ -33,3 +34,41 @@ function makePost() {
         });
     }
 }
+=======
+// script.js
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Function to handle modal submission
+function submitModal() {
+    var modalTextBoxValue = document.getElementById("modalTextBox").value;
+    alert("You entered: " + modalTextBoxValue);
+    closeModal(); // Close the modal after submission (you can modify this behavior)
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
+>>>>>>> 66b038fba1923bc2da0515200bf4f30a432aa92c
