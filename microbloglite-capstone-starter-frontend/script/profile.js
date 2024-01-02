510176f43d2f3793bc553372ae792fdfa0c37e9e
melzmatz <melendezdiana95@gmail.com>
2024-01-02 15:58:10 -0700

    fixed some logic

--- microbloglite-capstone-starter-frontend/script/profile.js
+++ microbloglite-capstone-starter-frontend/script/profile.js
@@ -1,36 +1,53 @@
-
+// instance of the AuthService class
 const authService = new AuthService();
 
-// => to fetch user information and populate the profile
+// function to check if the user is authenticated
+function isAuthenticated() {
+    const accessToken = window.localStorage.getItem("access-token");
+    return accessToken !== null;
+}
+
+// function to fetch user information and populate the profile
 async function fetchAndPopulateProfile() {
-    // ccheck if the user is logged in
-    if (authService.isLoggedIn()) {
+    // check if the user is authenticated
+    if (isAuthenticated()) {
         const loginData = authService.getLoginData();
         const username = loginData.username;
 
         try {
             // fetch user information from the API
-            const response = await fetch(`http://localhost:5000/api/users/${username}`);
+            const response = await fetch(`http://localhost:5000/api/users/${username}`, {
+                headers: {
+                    Authorization: `Bearer ${loginData.token}` // include the token 
+                }
+            });
             const userData = await response.json();
 
             if (response.status === 200) {
-                // pop the profile with user data
+                // pop the profile w/ user data
                 document.getElementById("fullName").textContent = userData.user.fullName;
                 document.getElementById("location").textContent = userData.user.location || "Location Not Provided";
                 document.getElementById("username").textContent = userData.user.username;
                 document.getElementById("aboutMe").textContent = userData.user.about || "About Me Not Provided";
             } else {
-                // when user data cannot be fetched
+                //case where user data cannot be fetched
                 console.error("Error fetching user data");
             }
         } catch (error) {
             console.error("Error fetching user data:", error);
         }
     } else {
-        // If the user is not logged in, redirect to the login page
+        // if the user is not authenticated, redirect to the login page/index
         window.location.href = "index.html";
     }
 }
 
-
+// Add an event listener to call the fetchAndPopulateProfile function when the DOM is loaded
 document.addEventListener("DOMContentLoaded", fetchAndPopulateProfile);
+
+// aadd an event listener for the logout button
+document.getElementById("logoutButton").addEventListener("click", function () {
+    // log out the user
+    authService.logout();
+});
+
