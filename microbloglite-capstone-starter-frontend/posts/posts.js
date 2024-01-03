ee00e5608dd8953370b2346a3c17618066969b28
Kyleemochoa01 <146892712+Kyleemochoa01@users.noreply.github.com>
2024-01-02 20:22:28 -0700

    added changes

--- microbloglite-capstone-starter-frontend/posts/posts.js
+++ microbloglite-capstone-starter-frontend/posts/posts.js
@@ -1,41 +1,7 @@
-/* Posts Page JavaScript */
-
-//"use strict";
-
-// script.js
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("openModalButton");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Function to open the modal
-function openModal() {
-    modal.style.display = "block";
+function createPost() {
+    const modal = document.getElementById("myModal")
+    const btn = document.getElementById("openModalButton");
+    const span = document
 }
-
-// Function to close the modal
-function closeModal() {
-    modal.style.display = "none";
-}
-
-// Function to handle modal submission
-function submitModal() {
-    var modalTextBoxValue = document.getElementById("modalTextBox").value;
-    alert("You entered: " + modalTextBoxValue);
-    closeModal(); // Close the modal after submission (you can modify this behavior)
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
 
 
